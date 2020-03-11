f14b782dd1317348430b89a874679cc583829a75
james <1204788939@qq.com>
2020-03-11 12:43:16 +0800

    update blog

--- blog-vuepress/docs/.vuepress/config.js
+++ blog-vuepress/docs/.vuepress/config.js
@@ -128,7 +128,7 @@ module.exports = {
                         '/views/fe-interview/UDP&TCP-different.md',
                         '/views/fe-interview/js-modules.md',
                         '/views/fe-interview/lazyMan.md',
-                        
+                        '/views/fe-interview/handwriting-promise.md',
                     ],
                 },
             ],
