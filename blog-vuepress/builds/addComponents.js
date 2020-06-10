abe77df6accf92d941c9c20c1ca414cc25ab6b27
james <1204788939@qq.com>
2020-06-10 11:16:53 +0800

    update blog

--- blog-vuepress/builds/addComponents.js
+++ blog-vuepress/builds/addComponents.js
@@ -10,5 +10,19 @@ function writeComponents(dir) {
             if (err) throw err;
             console.log(`add components to ${dir}`);
         });
+        /* fs.readFile(dir, "utf8", function(err, data) {
+            if (!err) {
+                //读取的内容
+                console.log('读取的内容', data)
+                if (!data.includes('文中若有不准确或错误的地方，欢迎指出，有兴趣可以的关注下')) {
+                    fs.appendFile(dir, `\n ## 最后
+
+                        文中若有不准确或错误的地方，欢迎指出，有兴趣可以的关注下[Github](https://github.com/GolderBrother)~ \n `, err => {
+                        if (err) throw err;
+                        console.log(`add components to ${dir}`);
+                    });
+                }
+            }
+        }); */
     }
 }
