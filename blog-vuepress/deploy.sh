6cc5eff1356a69db19ef953e16393abc52e10093
james <jamesezhang@tencent.com>
2020-11-15 18:38:21 +0800

    build: update deploy config

--- blog-vuepress/deploy.sh
+++ blog-vuepress/deploy.sh
@@ -28,8 +28,8 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 # git push -f git@github.com:GolderBrother/blog.git master:gh-pages
-# git push -f https://github.com/GolderBrother/blog.git master:gh-pages
+git push -f https://github.com/GolderBrother/blog.git master:gh-pages
 # git push -f git@github.com:GolderBrother/blog.git master:gh-pages
-git push -f git@gitee.com:GolderBrother/blog.git master:gh-pages
+# git push -f git@gitee.com:GolderBrother/blog.git master:gh-pages
 
 cd -
